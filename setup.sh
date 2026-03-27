9ff4ee7fa026c6fae575465e8b6cce5df15cfc63
vamshipriya14 <vamshipriya.konda@volibits.com>
2026-03-27 09:58:35 +0530

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Install Google Chrome stable (latest)
-wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y ./google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
