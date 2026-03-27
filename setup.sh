91edae8254f9e6ac9fb64722e36659ca5b23e16f
vamshipriya14 <vamshipriya.konda@volibits.com>
2026-03-27 09:41:24 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-python -m spacy download en_core_web_sm
+
+# Install Google Chrome stable (latest)
+wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
